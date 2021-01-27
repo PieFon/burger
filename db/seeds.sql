c214405d0fae4c844cc1b3f21b7d79a2f6776f87
Pierre Fontecha <Pierre.Fontecha@gmail.com>
2021-01-26 23:23:56 -0800

    updated configs

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom');
+INSERT INTO burgers (burger_name) VALUES ('Umami');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jamaican Jerk', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Filet', true);
+INSERT INTO burgers (burger_name) VALUES ('BBQ Sweet');
