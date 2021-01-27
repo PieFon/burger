b1f1e570d5cacc78dc6701ec976f08324982b336
Pierre Fontecha <Pierre.Fontecha@gmail.com>
2021-01-27 00:01:43 -0800

    updated heroku deployment code

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,11 @@
 ### Schema
 
-CREATE DATABASE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+-- creating the burgers table
 
 CREATE TABLE burgers
 (
